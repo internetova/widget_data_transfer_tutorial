5fb777a863de7bd51353fff2a209560ae628f5c4
Tatiana Sugina <internetova@yandex.ru>
2023-08-20 22:44:41 +0300

    добавлен пример с передачей параметров через конструктор

--- lib/features/search_filter/presentation/pages/filter_page.dart
+++ lib/features/search_filter/presentation/pages/filter_page.dart
@@ -40,33 +40,11 @@ class _FilterPageState extends State<FilterPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Wrap(
-              spacing: 8,
-              runSpacing: 8,
-              children: CategoryTypeEntity.values
-                  .map(
-                    (e) => ActionChip(
-                      avatar: _userCategories.contains(e)
-                          ? const Icon(Icons.check)
-                          : const Icon(Icons.remove),
-                      label: Text(e.name),
-                      backgroundColor:
-                          _userCategories.contains(e) ? Colors.green[200] : Colors.deepPurple[50],
-                      shape: const StadiumBorder(),
-                      onPressed: () => _onPressedCategory(e),
-                    ),
-                  )
-                  .toList(),
+            _CategoryList(
+              selectedCategories: _userCategories,
+              onPressedCategory: _onPressedCategory,
             ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: FilledButton(
-                  onPressed: _userCategories.isEmpty ? null : _saveFilter,
-                  child: const Text('Сохранить'),
-                ),
-              ),
-            ),
+            _SaveButton(saveFilter: _userCategories.isEmpty ? null : _saveFilter),
           ],
         ),
       ),
@@ -99,6 +77,77 @@ class _FilterPageState extends State<FilterPage> {
     Navigator.pop(
       context,
       SearchFilterEntity(_userCategories),
+    );
+  }
+}
+
+class _CategoryItem extends StatelessWidget {
+  final CategoryTypeEntity category;
+  final bool isSelected;
+  final ValueChanged<CategoryTypeEntity> onPressedCategory;
+
+  const _CategoryItem({
+    Key? key,
+    required this.category,
+    required this.isSelected,
+    required this.onPressedCategory,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ActionChip(
+      avatar: isSelected ? const Icon(Icons.check) : const Icon(Icons.remove),
+      label: Text(category.name),
+      backgroundColor: isSelected ? Colors.green[200] : Colors.deepPurple[50],
+      shape: const StadiumBorder(),
+      onPressed: () => onPressedCategory(category),
+    );
+  }
+}
+
+class _CategoryList extends StatelessWidget {
+  final List<CategoryTypeEntity> selectedCategories;
+  final ValueChanged<CategoryTypeEntity> onPressedCategory;
+
+  const _CategoryList({
+    Key? key,
+    required this.selectedCategories,
+    required this.onPressedCategory,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 8,
+      runSpacing: 8,
+      children: CategoryTypeEntity.values
+          .map(
+            (e) => _CategoryItem(
+              category: e,
+              isSelected: selectedCategories.contains(e),
+              onPressedCategory: onPressedCategory,
+            ),
+          )
+          .toList(),
+    );
+  }
+}
+
+class _SaveButton extends StatelessWidget {
+  final VoidCallback? saveFilter;
+
+  const _SaveButton({Key? key, this.saveFilter}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: FilledButton(
+          onPressed: saveFilter,
+          child: const Text('Сохранить'),
+        ),
+      ),
     );
   }
 }
